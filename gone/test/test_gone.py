b82234a6e2240e457c0484216c13b3b5ff6e629f
Aaron Magid <ahm64@case.edu>
2019-03-05 02:35:49 -0500

    test(Algorithm): Added several complex board test cases for the whole algorithm.

--- gone/test/test_gone.py
+++ gone/test/test_gone.py
@@ -1,6 +1,7 @@
+import pytest
 from gone.gone import Gone
 from gone.board import Board
-from utils import _generate_basic_test_board
+from utils import _generate_basic_test_board, _numbers_to_tile_types
 
 def test_gone_empty_board():
     input_board = [[]]
@@ -17,3 +18,65 @@ def test_gone_basic_board():
 
     assert results.max_rounds() == 1
     assert not results.any_black_remaining()
+
+def test_gone_some_black_remaining():
+    input_board = _numbers_to_tile_types([
+        [1, 1, 1],
+        [3, 3, 3],
+        [2, 2, 2]
+    ])
+
+    results = Gone(input_board)
+
+    assert results.max_rounds() == 0
+    assert results.any_black_remaining()
+
+TEST_BOARDS = [
+    (
+        [
+            [1, 2, 2, 2, 2]
+        ], 4, False
+    ),
+    (
+        [
+            [1, 2, 2, 2, 2, 2, 2, 2, 1]
+        ], 4, False
+    ),
+    (
+        [
+            [1, 2, 2, 2, 3, 2]
+        ], 3, True
+    ),
+    (
+        [
+            [2, 1, 1],
+            [2, 2, 2],
+            [2, 3, 3],
+            [3, 3, 2]
+        ], 3, True
+    ),
+    (
+        [
+            [1, 2, 2],
+            [3, 3, 2],
+            [2, 2, 2],
+            [2, 3, 3],
+            [2, 2, 2],
+            [3, 3, 2],
+            [2, 2, 2],
+            [2, 3, 3],
+            [2, 2, 2]
+        ], 18, False
+    )
+]
+@pytest.mark.parametrize(
+    'test_case', TEST_BOARDS
+)
+def test_gone_various_boards(test_case):
+    input_board, expected_rounds, expected_black_left = test_case
+    board = _numbers_to_tile_types(input_board)
+
+    results = Gone(board)
+
+    assert results.max_rounds() == expected_rounds
+    assert results.any_black_remaining() == expected_black_left
