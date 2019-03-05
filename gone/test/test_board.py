eb042410d00225d5d95d530e048659605a5d41f5
Aaron Magid <ahm64@case.edu>
2019-03-05 01:10:34 -0500

    test(Board): Moved test board generation to helper method.

--- gone/test/test_board.py
+++ gone/test/test_board.py
@@ -1,11 +1,14 @@
 from gone.board import Board
 
-def test_init_stores_input_board():
-    input_board = [
+def _generate_basic_test_board():
+    return [
         [1,1,1],
         [2,2,2],
         [3,3,3]
     ]
+
+def test_init_stores_input_board():
+    input_board = _generate_basic_test_board()
 
     board = Board(input_board)
 
