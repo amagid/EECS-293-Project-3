5507ae9a9c1a646c20845d43bd41a23f04fd7922
Aaron Magid <ahm64@case.edu>
2019-03-04 23:27:24 -0500

    feat(TileTypes): Move pseudocode over to py file.

---
+++ gone/tile_types.py
@@ -0,0 +1,7 @@
+TileTypes is an Enum representing the value contained in a tile on the board.
+Every cell in the 2-D board array must always contain exactly one TileType.
+
+Enum TileTypes
+    WHITE
+    BLACK
+    EMPTY
