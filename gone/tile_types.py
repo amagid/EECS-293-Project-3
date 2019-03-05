7fd0b3c0fcf4082e0131e77c9c02b6ea22360796
Aaron Magid <ahm64@case.edu>
2019-03-04 23:29:02 -0500

    feat(TileTypes): First pass at conversion to Python.

--- gone/tile_types.py
+++ gone/tile_types.py
@@ -1,7 +1,9 @@
-TileTypes is an Enum representing the value contained in a tile on the board.
-Every cell in the 2-D board array must always contain exactly one TileType.
+# TileTypes is an Enum representing the value contained in a tile on the board.
+# Every cell in the 2-D board array must always contain exactly one TileType.
 
-Enum TileTypes
-    WHITE
-    BLACK
-    EMPTY
+from enum import Enum
+
+class TileTypes(Enum):
+    WHITE = 1
+    BLACK = 2
+    EMPTY = 3
