e32d47aefcd4f8fc78277fcf0b01bcad7407ef5a
Aaron Magid <ahm64@case.edu>
2019-03-04 23:41:18 -0500

    feat(Board): Convert simple methods to python: init, tile_at, flip_tile.

--- gone/board.py
+++ gone/board.py
@@ -1,23 +1,29 @@
-Board is a class representing a game board with its tiles. Its internal _board
-property stores the current 2-D array representation of the board. The TYPE_AT
-method takes a SearchPosition and returns the TileType stored at the
-SearchPosition's coordinates. The FLIP_TILE method takes a SearchPosition and
-if the board contains a black tile at the SearchPosition's coordinates,
-replaces it with a white tile. The TO_SORTED_LINKED_LISTS method returns two
-doubly linked lists, white_tiles and black_tiles, which contain all of the 
-white and black tiles in the board, respectively.
+# Board is a class representing a game board with its tiles. Its internal _board
+# property stores the current 2-D array representation of the board. The TYPE_AT
+# method takes a SearchPosition and returns the TileType stored at the
+# SearchPosition's coordinates. The FLIP_TILE method takes a SearchPosition and
+# if the board contains a black tile at the SearchPosition's coordinates,
+# replaces it with a white tile. The TO_SORTED_LINKED_LISTS method returns two
+# doubly linked lists, white_tiles and black_tiles, which contain all of the 
+# white and black tiles in the board, respectively.
 
-Class Board
-    _board = a 2-D array containing TileTypes to represent the game board.
+from gone.search_position import SearchPosition
+from gone.tile_types import TileTypes
 
-    Constructor(2d_array_board)
-        Store the 2-D input array in _board
+class Board():
 
-    TILE_AT(search_position)
-        Return the TileType in _board at (search_position.x, search_position.y)
+    # Store the 2-D input array in _board
+    def __init__(self, board):
+        self._board = board
 
-    FLIP_TILE(search_position)
-        If _board contains a black tile at search_position, change it to white
+    # Return the TileType in _board at (search_position.x, search_position.y)
+    def tile_at(self, search_position):
+        return self._board[search_position.x][search_position.y]
+
+    # If _board contains a black tile at search_position, change it to white
+    def flip_tile(self, search_position):
+        if self._board[search_position.x][search_position.y] == TileTypes.BLACK:
+            self._board[search_position.x][search_position.y] = TileTypes.WHITE
 
     TO_SORTED_LINKED_LISTS()
         Let White_Tiles and Black_Tiles be empty doubly linked lists of
