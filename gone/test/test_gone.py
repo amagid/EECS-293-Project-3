a3224fa6ea075e613ebed5c6782ee76291b66527
Aaron Magid <ahm64@case.edu>
2019-03-05 02:20:11 -0500

    test(MainInterface): Added a few tests for Gone class.

--- gone/test/test_gone.py
+++ gone/test/test_gone.py
@@ -1,2 +1,19 @@
 from gone.gone import Gone
-from utils import _generate_basic_test_board, _numbers_to_tile_types
+from gone.board import Board
+from utils import _generate_basic_test_board
+
+def test_gone_empty_board():
+    input_board = [[]]
+
+    results = Gone(input_board)
+
+    assert results.max_rounds() == 0
+    assert not results.any_black_remaining()
+
+def test_gone_basic_board():
+    input_board, _ = _generate_basic_test_board()
+
+    results = Gone(input_board)
+
+    assert results.max_rounds() == 1
+    assert not results.any_black_remaining()
