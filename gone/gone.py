8701e98f7da4291348e81450328ce024fee30896
Aaron Magid <ahm64@case.edu>
2019-03-04 23:58:00 -0500

    feat(MainInterface): Moved pseudocode over to python file.

---
+++ gone/gone.py
@@ -0,0 +1,52 @@
+# The Gone class exposes methods necessary to process an input game board and
+# return the required number of rounds and whether or not any black pieces remain
+# at the end.
+
+# Class Gone
+    # _board = an instance of the Board class generated from the input array
+    # _white_tiles = a doubly linked list of SearchPositions (all white tiles)
+    # _black_tiles = a doubly linked list of SearchPositions (all black tiles)
+    # _max_rounds = the maximum number of rounds required to reach a black tile
+
+
+    # PROCESS_GAME(2d_array_board)
+        # Generate a new Board instance from 2d_array_board and store in _board
+        # _white_tiles, _black_tiles = _board.TO_SORTED_LINKED_LISTS()
+
+        # SIMULATE_GAME()
+
+        # Return MAX_ROUNDS(), ANY_BLACK_REMAINING()
+
+
+    # SIMULATE_GAME()
+        # While _white_tiles is not empty
+            # Let tile be the first element in _white_tiles
+            # Remove tile from _white_tiles
+
+            # ADD_NEIGHBORS_TO_NEXT_ROUND(tile)
+
+            # Set _max_rounds = max(_max_rounds, tile.rounds)
+
+        # Return _max_rounds
+
+        
+    # ADD_NEIGHBORS_TO_NEXT_ROUND(tile)
+        # Let neighbors contain the valid SearchPositions adjacent to tile
+
+        # For each neighbor_position in neighbors
+            # If _board.TILE_AT(neighbor_position) is TileTypes.BLACK
+                # _board.FLIP_TILE(neighbor_position)
+                # Set neighbor_position.rounds = tile.rounds + 1
+                # Add neighbor_position to end of _white_tiles 
+
+
+    # MAX_ROUNDS()
+        # Return _max_rounds
+
+
+    # ANY_BLACK_REMAINING()
+        # For each position in _black_tiles
+            # If _board.TILE_AT(position) is TileTypes.BLACK
+                # Return true
+
+        # Return false
