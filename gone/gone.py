d228cb706d857ed653e5c807fa50c9de9792beff
Aaron Magid <ahm64@case.edu>
2019-03-19 04:33:55 -0400

    chore(Cleanup): Removed obsolete TODO: make Gone methods static.

--- gone/gone.py
+++ gone/gone.py
@@ -9,8 +9,6 @@ from gone.tile_types import TileTypes
 
 class Gone():
 
-    # TODO: Maybe reimplement this as static to allow for better unit testing
-
     def __init__(self, array_board):
         # an instance of the Board class generated from the input array
         self._board = Board(array_board)
